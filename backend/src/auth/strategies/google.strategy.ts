c9715e37ff08d807b2347732701e0a3e82d1de3e
jimin <jimin>
2025-09-13 01:50:22 +0900

    Added .prettierignore file

--- backend/src/auth/strategies/google.strategy.ts
+++ backend/src/auth/strategies/google.strategy.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-google-oauth20'
 
 
+@Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
